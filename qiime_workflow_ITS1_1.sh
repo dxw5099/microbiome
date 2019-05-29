cc8e0e35ace5a8c8cc042b52276e2aa1c0e14c7e
dxw5099 <dxw5099@gmail.com>
2019-05-29 15:46:50 -0700

    Update qiime_workflow_ITS1_1.sh

--- qiime_workflow_ITS1_1.sh
+++ qiime_workflow_ITS1_1.sh
@@ -5,7 +5,7 @@
 ##### Perform alignment #####
 #source activate qiime1
 #$ -v PATH=/hpc/apps/python27/externals/toolshed/0.4.0/bin:/common/genomics-core/anaconda2/envs/qiime1/bin:/common/genomics-core/anaconda2/condabin:/common/genomics-core/apps/.bds:/common/genomics-core/anaconda2/bin/:/opt/xcat/bin:/opt/xcat/sbin:/opt/xcat/share/xcat/tools:/opt/sge/bin:/opt/sge/bin/lx-amd64:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/wud3/.local/bin:/home/wud3/bin
-
-parallel_pick_otus_blast.py -i ITS.fna  -b /common/genomics-core/reference/Microbiome/THFv1.61.sort.fasta -O 120 -o otus_THFv1.61
+cat fasta_ITS/*_ITS1.fasta > fasta_ITS/ITS.fna
+parallel_pick_otus_blast.py -i fasta_ITS/ITS.fna  -b /common/genomics-core/reference/Microbiome/THFv1.61.sort.fasta -O 120 -o otus_THFv1.61
 #/common/genomics-core/anaconda2/envs/qiime1/bin/parallel_pick_otus_blast.py -i 16S.fna  -b /common/genomics-core/reference/Microbiome/gg_13_5_97_otus.fasta -O 120 -o otus_gg_13_5_97
 #parallel_pick_otus_blast.py -i 16S.fna  -b /common/genomics-core/reference/Microbiome/gg_13_5_97_otus.fasta -O 120 -o otus_gg_13_5_97
